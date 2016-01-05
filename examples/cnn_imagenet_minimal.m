1e1447d02f72aefcf982fa3e80d7ee0a8de5d46c
Wang Lei <rexwng@gmail.com>
2016-01-05 22:14:15 +0800

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master' into feature-windows"
    This reverts commit bdaabadd9f55497c442afbed9da0e80431668e11.

--- examples/cnn_imagenet_minimal.m
+++ examples/cnn_imagenet_minimal.m
@@ -3,11 +3,10 @@ function cnn_imagenet_minimal()
 
 % setup toolbox
 run(fullfile(fileparts(mfilename('fullpath')), ...
-  '..', '..', 'matlab', 'vl_setupnn.m')) ;
+  '..', 'matlab', 'vl_setupnn.m')) ;
 
 % download a pre-trained CNN from the web
 if ~exist('imagenet-vgg-f.mat', 'file')
-  fprintf('Downloading a model ... this may take a while\n') ;
   urlwrite('http://www.vlfeat.org/matconvnet/models/imagenet-vgg-f.mat', ...
     'imagenet-vgg-f.mat') ;
 end
@@ -16,8 +15,8 @@ net = load('imagenet-vgg-f.mat') ;
 % obtain and preprocess an image
 im = imread('peppers.png') ;
 im_ = single(im) ; % note: 255 range
-im_ = imresize(im_, net.meta.normalization.imageSize(1:2)) ;
-im_ = im_ - net.meta.normalization.averageImage ;
+im_ = imresize(im_, net.normalization.imageSize(1:2)) ;
+im_ = im_ - net.normalization.averageImage ;
 
 % run the CNN
 res = vl_simplenn(net, im_) ;
@@ -27,5 +26,5 @@ scores = squeeze(gather(res(end).x)) ;
 [bestScore, best] = max(scores) ;
 figure(1) ; clf ; imagesc(im) ;
 title(sprintf('%s (%d), score %.3f',...
-   net.meta.classes.description{best}, best, bestScore)) ;
+   net.classes.description{best}, best, bestScore)) ;
 
--- examples/cnn_mnist_init.m
+++ examples/cnn_mnist_init.m
@@ -1,7 +1,6 @@
 function net = cnn_mnist_init(varargin)
 % CNN_MNIST_LENET Initialize a CNN similar for MNIST
-opts.useBatchNorm = true ;
-opts.networkType = 'simplenn' ;
+opts.useBnorm = true ;
 opts = vl_argparse(opts, varargin) ;
 
 rng('default');
@@ -39,31 +38,10 @@ net.layers{end+1} = struct('type', 'conv', ...
 net.layers{end+1} = struct('type', 'softmaxloss') ;
 
 % optionally switch to batch normalization
-if opts.useBatchNorm
+if opts.useBnorm
   net = insertBnorm(net, 1) ;
   net = insertBnorm(net, 4) ;
   net = insertBnorm(net, 7) ;
-end
-
-% Meta parameters
-net.meta.inputSize = [27 27 1] ;
-net.meta.trainOpts.learningRate = 0.001 ;
-net.meta.trainOpts.numEpochs = 20 ;
-net.meta.trainOpts.batchSize = 100 ;
-
-% Fill in defaul values
-net = vl_simplenn_tidy(net) ;
-
-% Switch to DagNN if requested
-switch lower(opts.networkType)
-  case 'simplenn'
-    % done
-  case 'dagnn'
-    net = dagnn.DagNN.fromSimpleNN(net, 'canonicalNames', true) ;
-    net.addLayer('error', dagnn.Loss('loss', 'classerror'), ...
-             {'prediction','label'}, 'error') ;
-  otherwise
-    assert(false) ;
 end
 
 % --------------------------------------------------------------------
@@ -73,7 +51,7 @@ assert(isfield(net.layers{l}, 'weights'));
 ndim = size(net.layers{l}.weights{1}, 4);
 layer = struct('type', 'bnorm', ...
                'weights', {{ones(ndim, 1, 'single'), zeros(ndim, 1, 'single')}}, ...
-               'learningRate', [1 1 0.05], ...
+               'learningRate', [1 1], ...
                'weightDecay', [0 0]) ;
 net.layers{l}.biases = [] ;
 net.layers = horzcat(net.layers(1:l), layer, net.layers(l+1:end)) ;
